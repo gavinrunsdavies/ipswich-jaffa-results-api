15457ca7fa17ecbb51f43a464622875bc645af88
Gavin Davies <gavinrunsdavies@gmail.com>
2025-05-01 10:41:19 +0100

    Create Badges.php

---
+++ V2/Badges/Badges.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace IpswichJAFFARunningClubAPI\V2\Badges;
+
+abstract class Badges
+{
+    const COMMITTEE = 1;
+    const COACH = 2;
+    const TRACK = 3;
+    const CROSS_COUNTRY = 4;
+    const MARATHON = 5;
+    const INTERNATIONAL = 6;    
+}
