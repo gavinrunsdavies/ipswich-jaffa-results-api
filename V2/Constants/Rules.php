c8a54949914acd5df17787837b2097265e198e4b
Gavin Davies <gavinrunsdavies@gmail.com>
2025-05-23 15:31:15 +0100

    Add date for 2025 WMA gradings

--- V2/Constants/Rules.php
+++ V2/Constants/Rules.php
@@ -5,4 +5,5 @@ namespace IpswichJAFFARunningClubAPI\V2\Constants;
 abstract class Rules
 {
     const START_OF_2015_AGE_GRADING = '2017-01-01';
+    const START_OF_2025_AGE_GRADING = '2025-04-01';
 }
