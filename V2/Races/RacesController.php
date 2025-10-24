1301c625cef4dc1777331736f86a661ad699d294
Gavin Davies <gavinrunsdavies@gmail.com>
2025-10-24 15:07:24 +0100

    Add date parameter to getHistoricRaces method

--- V2/Races/RacesController.php
+++ V2/Races/RacesController.php
@@ -92,7 +92,13 @@ class RacesController extends BaseController implements IRoute
 
 		register_rest_route($this->route, '/races/history', array(
 			'methods'             => \WP_REST_Server::READABLE,
-			'callback'            => array($this, 'getHistoricRaces')
+			'callback'            => array($this, 'getHistoricRaces'),
+			'args'                => array(
+				'date'           => array(
+					'required'          => false,
+					'validate_callback' => array($this, 'isValidDate')
+				)
+			)
 		));
 
 		// Update Race - two routes
@@ -207,7 +213,8 @@ class RacesController extends BaseController implements IRoute
 
 	public function getHistoricRaces(\WP_REST_Request $request)
 	{
-		return rest_ensure_response($this->command->getHistoricRaces());
+		$parameters = $request->get_query_params();
+		return rest_ensure_response($this->command->getHistoricRaces($parameters['date']));
 	}
 
 	public function isValidRaceUpdateField(string $value, $request, string $key)
